0f34627890656207a21cf13a3391877b7b3f3a56
RAHUL R <rahulsquads@gmail.com>
2023-03-18 17:36:21 +0530

    minor improvements

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
                 </div>
                 <About />
                 <Experience />
-                <Tech />
+                {/* <Tech /> */}
                 <Works />
                  
                 <div className="relative z-0">
